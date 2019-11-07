b97748214c14afd336f36b665ebe7500998955e4
Lubo Grozdanov <l.grozdanov@stenik.bg>
2019-11-07 15:28:24 +0200

    Initial commit

---
+++ locust_file.py
@@ -0,0 +1,20 @@
+from locust import HttpLocust, Locust, TaskSet, task
+from links import *
+import random
+
+class UserBehavior(TaskSet):
+  link = "/"
+
+  def on_start(self):
+            if len(LINKS) > 0:
+                self.link = random.choice(LINKS)
+
+  @task(1)
+  def my_task(self):
+      self.client.get(self.link)
+
+class WebsiteUser(HttpLocust):
+  task_set = UserBehavior
+  min_wait = 20000
+  max_wait = 30000
+
